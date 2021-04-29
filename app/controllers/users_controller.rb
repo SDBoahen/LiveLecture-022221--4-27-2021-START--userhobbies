c519725d3c71cc7b5791b5237869dad0c02cb38b
SDBoahen <SDBoahen@gmail.com>
2021-04-29 07:43:25 -0400

    END OF LECTURE == 4-28-2021

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,18 @@
 class UsersController < ApplicationController
 
 
+    before_action :find_user, only: [:show, :edit]
+
+
+
+
     def index
         @users = User.all
     end
     def show
-        @user = User.find_by_id(params[:id])
     end
+
+
 
 
     # "Form Getterz"
@@ -15,16 +21,45 @@ class UsersController < ApplicationController
     end
     def create
         binding.pry
+        @user = User.new(user_params)
+        if @user.save
+            binding.pry
+            redirect_to user_path(@user)
+        else
+            render :new
+        end
+
     end 
     # "Validatorzzzz"
 
+
     
+
     # "Form Getterz"
     def edit
+        binding.pry
     end
     def update
+        binding.pry
     end 
     # "Validatorzzzz"
+
+
+
+
+    private 
+
+
+    def find_user
+        @user = User.find_by_id(params[:id])
+    end
+
+
+    def user_params
+        params.require(:user).permit(:name, :username)
+        #WHO??                  WHAT??
+    end
+
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,26 @@ Rails.application.routes.draw do
 
   # resources :users
 
-    resources :users
+    # resources :users #ONLY !! *** , except: [:index, :show]
 
-  # #index  
-  # get '/users', to: "users#index", as:"users" 
+  #INDEX  
+  get '/users', to: "users#index", as:"users" 
 
-  # #show  
-  # get '/user/:id', to: "users#show", as:"user" 
+
+  #NEW
+  get '/users/new', to: "users#new", as:"new_user"
+
+  #CREATE
+  post '/users', to: "users#create"
+
+
+
+  #SHOW  
+  get '/users/:id', to: "users#show", as:"user"
+
+
+
+
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
